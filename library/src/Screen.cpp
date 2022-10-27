ff0aa25ba0b5c92d145db2bf3bd325468e0971e5
Tyler Gilbert <tyler.w.gilbert@gmail.com>
2022-10-26 19:26:17 -0500

    Fix build with Stratify OS and add minor cosmetic improvements to Screen. cherry-pick:main

--- library/src/Screen.cpp
+++ library/src/Screen.cpp
@@ -3,33 +3,36 @@
 
 using namespace lvgl;
 
-var::Vector<lv_obj_t*> Screen::m_screen_list;
+namespace {
+using ScreenList = var::Vector<lv_obj_t *>;
+auto local_screen_list = ScreenList();
+}
 
-Screen::Screen(const char * name) {
-  if( m_screen_list.count() == 0 ){
-    //add the default screen
+Screen::Screen(const char *name) {
+  if (local_screen_list.count() == 0) {
+    // add the default screen
     auto active_screen = api()->disp_get_scr_act(nullptr);
-    m_screen_list.push_back(active_screen);
+    local_screen_list.push_back(active_screen);
     set_user_data(active_screen, default_name);
   }
 
   m_object = api()->obj_create(nullptr);
   set_user_data(m_object, name);
-  for(auto & entry: m_screen_list){
-    if( entry == nullptr ){
+  for (auto &entry : local_screen_list) {
+    if (entry == nullptr) {
       entry = m_object;
       return;
     }
   }
-  m_screen_list.push_back(m_object);
+  local_screen_list.push_back(m_object);
 }
 
 void Screen::remove_screen(const char *name) {
-  for(auto & entry: m_screen_list){
-    if( entry != nullptr ){
+  for (auto &entry : local_screen_list) {
+    if (entry != nullptr) {
       auto s = Screen(entry);
       const var::StringView s_name = s.name();
-      if( s_name == name && s_name != default_name){
+      if (s_name == name && s_name != default_name) {
         s.async_remove();
         entry = nullptr;
         return;
@@ -39,13 +42,15 @@ void Screen::remove_screen(const char *name) {
 }
 
 Screen Screen::find_screen(const char *name) {
-  for(auto & entry: m_screen_list){
-    if( entry != nullptr ){
+  for (auto &entry : local_screen_list) {
+    if (entry != nullptr) {
       auto s = Screen(entry);
-      if( var::StringView(s.name()) == name ){
+      if (var::StringView(s.name()) == name) {
         return s;
       }
     }
   }
-  return Screen((lv_obj_t*)nullptr);
+  return Screen{static_cast<lv_obj_t *>(nullptr)};
 }
+
+var::Vector<lv_obj_t *> &Screen::screen_list() { return local_screen_list; }
